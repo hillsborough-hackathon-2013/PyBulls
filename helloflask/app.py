22f4173bb5461b8840c9e726ef3f0ce1949cf2fb
dandye <dye.daniel@gmail.com>
2013-03-22 21:58:43 -0400

    testing postgres and psycopg2

--- helloflask/app.py
+++ helloflask/app.py
@@ -1,11 +1,22 @@
 import os
+import psycopg2
 from flask import Flask
 
 app = Flask(__name__)
 
+CONN = psycopg2.connect(database="d7tqum8ti79ms",
+                        user="oivlwelvilidjf",
+                        password="DSbFeB1X3knNMeQw-7hpXnbUHG",
+                        host="ec2-54-243-125-2.compute-1.amazonaws.com"
+                        )
+CUR = CONN.cursor()
+
+
 @app.route('/')
 def hello():
-    return 'Hello Heroku!'
+    CUR.execute("""SELECT * FROM members LIMIT 5;""")
+    record = CUR.next()
+    return 'Hello Heroku! Here is some DB: {}'.format(record)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
