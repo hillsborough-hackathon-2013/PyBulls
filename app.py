25805859c5784b606f285e8c381ddee25af3349f
Chong Kim <chongkim@yahoo.com>
2013-04-14 16:13:26 -0400

    pants

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ HEADER = """<html>
 @app.route('/pants')
 def pants():
     return HEADER + """<body>Pants!<br/>
-    <img src="/pants.jpg"/>
+    <img src="/static/pants.jpg"/>
 </body>
 </html>
     """
