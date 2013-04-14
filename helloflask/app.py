d9eb0d4f0fdfa057aaf94dcc695d5c0c564ea608
dandye <dye.daniel@gmail.com>
2013-04-13 21:00:50 -0400

    hard coded IFrame

--- helloflask/app.py
+++ helloflask/app.py
@@ -16,7 +16,17 @@ CUR = CONN.cursor()
 def hello():
     CUR.execute("""SELECT * FROM members LIMIT 5;""")
     record = CUR.next()
-    return 'Hello Heroku! Here is some DB: {}'.format(record)
+    webmap="240fc14f85eb4ca2949074b8ff7dcbbb"
+    return 'HTML = """<html>
+<head>
+<title>Neighborhood Watch Information System</title>
+</head>
+<body>
+<h1>Neighborhood Watch Information System</h1>
+<iframe width="500" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.arcgis.com/home/webmap/embedViewer.html?webmap={webmap}&amp;extent=-82.4591,28.0088,-82.4389,28.0217"></iframe><br /><small><a href="http://www.arcgis.com/home/webmap/viewer.html?webmap={webmap}&amp;extent=-82.4591,28.0088,-82.4389,28.0217" style="color:#0000FF;text-align:left" target="_blank">View Larger Map</a></small>
+</body>
+</html>
+""" {}'.format(webmap=webmap)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
