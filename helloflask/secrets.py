83e8ce71819a599229bccd746b3389e51b79a55f
dandye <dye.daniel@gmail.com>
2013-04-13 20:57:04 -0400

    passwords

---
+++ helloflask/secrets.py
@@ -0,0 +1,7 @@
+
+class Secrets:
+    def __init__(self):
+        self.database="d7tqum8ti79ms"
+        self.user="oivlwelvilidjf"
+        self.password="DSbFeB1X3knNMeQw-7hpXnbUHG"
+        self.host="ec2-54-243-125-2.compute-1.amazonaws.com"
