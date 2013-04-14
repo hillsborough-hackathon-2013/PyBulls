3b06e51164093e16bd736e6c8ddc26eb0e5e76eb
dandye <dye.daniel@gmail.com>
2013-04-13 22:13:19 -0400

    style

--- app.py
+++ app.py
@@ -31,9 +31,99 @@ def hello():
     return """<html>
 <head>
 <title>Neighborhood Watch Information System</title>
+<style>
+    body{
+        font-family: 'Verdana';
+    }
+    .dash{
+        background-color: rgb(255, 255, 255);
+        color: rgb(255, 255, 255);
+        cursor: auto;
+        display: inline-block;
+        font-family: Raleway, 'Open Sans', Segoe, 'Helvetica Neue', Helvetica, Arial, sans-serif;
+        font-size: 23px;
+        font-weight: 800;
+        height: 1px;
+        letter-spacing: 3px;
+        line-height: 27px;
+        text-align: center;
+        text-transform: uppercase;
+        transition-delay: 0s, 0s;
+        transition-duration: 0.3s, 0.3s;
+        transition-property: color, background-color;
+        transition-timing-function: ease-out, ease-out;
+        vertical-align: middle;
+        width: 70px;
+    }    
+    .w0{
+        color: rgb(216, 205, 222);
+        cursor: auto;
+        display: inline;
+        font-family: Raleway, 'Open Sans', Segoe, 'Helvetica Neue', Helvetica, Arial, sans-serif;
+        font-size: 23px;
+        font-weight: 300;
+        height: auto;
+        letter-spacing: 3px;
+        line-height: 27px;
+        text-align: center;
+        text-transform: uppercase;
+        transition-delay: 0s, 0s;
+        transition-duration: 0.3s, 0.3s;
+        transition-property: color, background-color;
+        transition-timing-function: ease-out, ease-out;
+        width: auto;
+    }
+    
+    #hackhillslogo{
+        color: rgb(216, 205, 222);
+        background: rgba(2,43,66,0.7);
+        font-size: 22.5px;
+        line-height: 27px;
+        letter-spacing: 3px;
+        padding: 0 0 10px;
+        font-family: Raleway, 'Open Sans', Segoe, 'Helvetica Neue', Helvetica, Arial, sans-serif;
+        font-size: 23px;
+        font-weight: 800;
+        height: auto;
+        letter-spacing: 3px;
+        line-height: 27px;
+        text-align: center;
+        text-transform: uppercase;
+        transition-delay: 0s, 0s;
+        transition-duration: 0.3s, 0.3s;
+        transition-property: color, background-color;
+        transition-timing-function: ease-out, ease-out;
+        width: auto;
+        
+}
+    }
+    #mybox{
+        border: 10px solid black;
+        border-radius:15px;
+        display: inline-block;
+    };
+    
+</style>
+
 </head>
 <body>
+<body>
+<center>
+
+
+<h1 id="hackhillslogo">
+<b>
+  <span class="dash"></span><span class="w0"> 2013 </span><span class="dash"></span> <br>
+  <span class="w3">Hillsborough</span>
+  <span class="w3">Hackathon</span>
+</b>
+</h1>
+
+
+
 <h1>Neighborhood Watch Information System</h1>
+<div id="mybox">
+
 <center>
 <iframe width="500" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.arcgis.com/home/webmap/embedViewer.html?webmap={webmap}&amp;extent=-82.4591,28.0088,-82.4389,28.0217"></iframe><br /><small><a href="http://www.arcgis.com/home/webmap/viewer.html?webmap={webmap}&amp;extent=-82.4591,28.0088,-82.4389,28.0217" style="color:#0000FF;text-align:left" target="_blank">View Larger Map</a></small>
 </center>
